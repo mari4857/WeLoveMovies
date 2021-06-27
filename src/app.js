9a254da0603453aa801dab8b6916d33e36e82b07
mari4857 <33039256+mari4857@users.noreply.github.com>
2021-06-27 13:35:20 -0400

    adding reviews router, controller and service.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 
 const moviesRouter = require("./movies/movies.router");
+const reviewsRouter = require("./reviews/reviews.router");
 
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
 
 app.use(notFound);
 app.use(errorHandler);
---
+++ src/reviews/reviews.service.js
@@ -0,0 +1,64 @@
+const knex = require("../db/connection");
+
+function read(review_id) {
+  return knex("reviews")
+    .select("*")
+    .where({ "reviews.review_id": review_id })
+    .first();
+}
+
+function update(updatedReview) {
+  return knex("reviews")
+    .select("*")
+    .where({ review_id: updatedReview.review_id })
+    .update(updatedReview, "*");
+}
+
+function addCritic(reviews) {
+  return reviews.map((review) => {
+    return {
+      review_id: review.review_id,
+      content: review.content,
+      score: review.score,
+      created_at: review.created_at,
+      updated_at: review.updated_at,
+      critic_id: review.critic_id,
+      movie_id: review.movie_id,
+      critic: {
+        critic_id: review.critic_id,
+        preferred_name: review.preferred_name,
+        surname: review.surname,
+        organization_name: review.organization_name,
+        created_at: review.c_created_at,
+        updated_at: review.c_updated_at,
+      },
+    };
+  });
+}
+
+function readWithCritic(review_id) {
+  return knex("reviews")
+    .join("critics", "reviews.critic_id", "critics.critic_id")
+    .select(
+      "reviews.*",
+      "critics.created_at as c_created_at",
+      "critics.updated_at as c_updated_at",
+      "critics.critic_id as c_critic_id",
+      "critics.preferred_name",
+      "critics.surname",
+      "critics.organization_name"
+    )
+    .where({ "reviews.review_id": review_id })
+    .then(addCritic);
+}
+
+function destroy(review_id) {
+  return knex("reviews").where({ review_id }).del();
+}
+
+module.exports = {
+  read,
+  update,
+  readWithCritic,
+  destroy,
+};
